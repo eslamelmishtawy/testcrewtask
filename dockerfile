b66310533b49e8f67781dda39103213942f5ec58
Islam Elmishtawy <islam.hossam@tempo.fit>
2023-10-11 17:44:57 +0300

    Update dockerfile and DriverFactory.java

--- dockerfile
+++ dockerfile
@@ -1,8 +1,15 @@
-FROM jenkins/jenkins:jdk21
+FROM --platform=linux/amd64 jenkins/jenkins:jdk21
 
 USER root
 
 RUN apt-get update \
  && apt-get install maven -y
+
+RUN apt-get update && apt-get install -y wget
+RUN wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
+RUN echo "deb http://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google.list
+RUN apt-get update
+RUN apt-get install -y google-chrome-stable xvfb
+ENV CHROME_BIN='/usr/bin/google-chrome-stable'
 
 USER jenkins
--- src/main/java/com/driverfactory/DriverFactory.java
+++ src/main/java/com/driverfactory/DriverFactory.java
@@ -3,6 +3,7 @@ package com.driverfactory;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 
@@ -13,8 +14,11 @@ public class DriverFactory {
     public WebDriver init_driver(String browser)
     {
         if(browser.equals("chrome")){
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--headless");
             WebDriverManager.chromedriver().setup();
-            tlDriver.set(new ChromeDriver());
+            tlDriver.set(new ChromeDriver(options));
+
         } else if (browser.equals("firefox")) {
             WebDriverManager.firefoxdriver().setup();
             tlDriver.set(new FirefoxDriver());
